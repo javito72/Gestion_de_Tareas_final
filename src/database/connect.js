ed7b11d4c93bed7a78dc295559155e113f089430
Chris72 <93304927+javito72@users.noreply.github.com>
2024-11-22 20:30:45 -0300

    correciones para deployar

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,17 +1,13 @@
-// src/database/connect.js
 const mongoose = require('mongoose');
 require('dotenv').config(); // Cargar las variables de entorno
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI); // Conexión simplificada
     console.log('Conectado a MongoDB Atlas');
   } catch (error) {
     console.error('Error al conectar a MongoDB Atlas:', error);
-    process.exit(1);
+    process.exit(1); // Finaliza el proceso en caso de error
   }
 };
 
